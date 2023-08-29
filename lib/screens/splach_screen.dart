867e3e1e07a549668e0254ba5c5f9dac55d656ca
Abdullah <abdallahaljehani926@gmail.com>
2023-08-29 22:36:38 +0200

    Add Splash-Screen + Models

---
+++ lib/screens/splach_screen.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:marvel_app/helpers/const.dart';
+import 'package:marvel_app/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(context,
+          CupertinoPageRoute(builder: (context) => const HomeScreen()));
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // this size variable will dynamically take the size of the screen of the user.
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Center(
+        child: Image.asset(
+          'assets/inverted_logo.png',
+          width: size.width *
+              .80, // its like you saying , (i want to this image to take 80% percentage of the width of the screen )
+        ),
+      ),
+    );
+  }
+}
